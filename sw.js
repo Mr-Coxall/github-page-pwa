e01e0f475699a65bfecac8c969bfe87d80a34e9f
Mr-Coxall <Mr-Coxall@users.noreply.github.com>
2022-03-29 19:58:28 +0000

    Prettified Code!

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-var GHPATH = '/github-page-pwa'
-var APP_PREFIX = 'gppwa_'
-var VERSION = 'version_001'
+var GHPATH = "/github-page-pwa"
+var APP_PREFIX = "gppwa_"
+var VERSION = "version_001"
 var URLS = [
   `${GHPATH}/`,
   `${GHPATH}/index.html`,
@@ -10,31 +10,31 @@ var URLS = [
 ]
 
 var CACHE_NAME = APP_PREFIX + VERSION
-self.addEventListener('fetch', function (e) {
-  console.log('Fetch request : ' + e.request.url)
+self.addEventListener("fetch", function (e) {
+  console.log("Fetch request : " + e.request.url)
   e.respondWith(
     caches.match(e.request).then(function (request) {
       if (request) {
-        console.log('Responding with cache : ' + e.request.url)
+        console.log("Responding with cache : " + e.request.url)
         return request
       } else {
-        console.log('File is not cached, fetching : ' + e.request.url)
+        console.log("File is not cached, fetching : " + e.request.url)
         return fetch(e.request)
       }
     })
   )
 })
 
-self.addEventListener('install', function (e) {
+self.addEventListener("install", function (e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function (cache) {
-      console.log('Installing cache : ' + CACHE_NAME)
+      console.log("Installing cache : " + CACHE_NAME)
       return cache.addAll(URLS)
     })
   )
 })
 
-self.addEventListener('activate', function (e) {
+self.addEventListener("activate", function (e) {
   e.waitUntil(
     caches.keys().then(function (keyList) {
       var cacheWhitelist = keyList.filter(function (key) {
@@ -44,7 +44,7 @@ self.addEventListener('activate', function (e) {
       return Promise.all(
         keyList.map(function (key, i) {
           if (cacheWhitelist.indexOf(key) === -1) {
-            console.log('Deleting cache : ' + keyList[i])
+            console.log("Deleting cache : " + keyList[i])
             return caches.delete(keyList[i])
           }
         })
