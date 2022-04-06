b9ec552662c15d5132cf77c755ba03df6d9313db
Patrick Coxall <mr.coxall@mths.ca>
2022-04-06 14:35:31 -0400

    moved sw

--- js/script.js
+++ js/script.js
@@ -5,13 +5,15 @@
 // This file contains the JS functions for index.html
 
 /**
- * This function displays an alert.
+ * Check servie worker.
  */
-
 if (navigator.serviceWorker) {
   navigator.serviceWorker.register("/github-page-pwa/sw.js", { scope: "/github-page-pwa/" })
 }
 
+/**
+ * This function displays an alert.
+ */
 function myButtonClicked() {
   document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
 }
